f10d26a5975e4f0140a6fba95de584be400c3c55
Ories <tellt1@126.com>
2022-04-24 10:53:21 +0800

    init

---
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -0,0 +1,12 @@
+package com.github.hcsp;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeTest {
+    @Test
+    public void homeHasTwoKindsOfCat() {
+        Home home = new Home();
+        home.cat1 = new com.github.hcsp.pet1.Cat();
+        home.cat2 = new com.github.hcsp.pet2.Cat();
+    }
+}
